9a88947accaae1a15738e1cd247854924e5a02f7
ShinYi Tan <shinxyi.tan@gmail.com>
2017-02-10 15:42:25 -0600

    Initiated Reviews App.

--- ConsumerAffairsAPI/settings.py
+++ ConsumerAffairsAPI/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'apps.loginreg',
     'apps.companies',
+    'apps.reviews',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- ConsumerAffairsAPI/urls.py
+++ ConsumerAffairsAPI/urls.py
@@ -4,5 +4,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^users/', include('apps.loginreg.urls', namespace="users")),
-    url(r'^companies/', include('apps.companies.urls', namespace="companies"))
+    url(r'^companies/', include('apps.companies.urls', namespace="companies")),
+    url(r'^reviews/', include('apps.reviews.urls', namespace="reviews"))
+
 ]
